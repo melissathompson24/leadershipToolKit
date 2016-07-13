8d9e07e89b442e942017e74549d801c4856c9894
melissathompson24 <melissa.thompson24@gmail.com>
2016-07-13 16:04:21 -0600

    printable pages added

---
+++ js/print.js
@@ -0,0 +1,13 @@
+// printed page footer 
+
+document.getElementById("footer").innerHTML =
+  "<div>"
+  +  "<p>Learning \& Living Leadership: A Tool Kit | www.ToolKit.NCWWI.org</p>"
+  +  "<p>National Child Welfare Workforce Institute | www.NCWWI.org</a> | www.MyNCWWI.org</p>"
+  +  "<p>Copyright &copy;" + new Date().getFullYear() + ". All Rights Reserved.</span></p>"
+  +  "</div>"
+  ;
+
+
+
+
